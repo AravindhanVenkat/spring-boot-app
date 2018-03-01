3b8658bf7944d46067b2f29fa3ebf1a54aaa4e66
Aravindhan <aravindhan.venkat@gmail.com>
2018-03-01 23:37:42 +0530

    Spring Boot First commit

---
+++ src/main/java/com/shipment/tracking/TrackShipmentApplication.java
@@ -0,0 +1,12 @@
+package com.shipment.tracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackShipmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackShipmentApplication.class, args);
+	}
+}
---
+++ src/main/java/com/shipment/tracking/controller/HomeController.java
@@ -0,0 +1,25 @@
+package com.shipment.tracking.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HomeController {
+
+	@RequestMapping(value="/api/call", method = RequestMethod.GET)
+	public ResponseEntity<Map<String,String>> getLogin(){
+		Map<String,String> result = new HashMap<String,String>();
+		result.put("tamil","90");
+		result.put("maths","90");
+		result.put("science","90");
+		result.put("social","90");
+		return new ResponseEntity<Map<String,String>>(result,HttpStatus.OK);
+	}
+}
